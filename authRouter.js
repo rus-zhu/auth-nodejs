1a23746e74be817606cadde27f8a8006b1eeb78d
rus-zhu <microlab982@yandex.ru>
2022-12-01 22:09:32 +0500

    Implement registration and validation(username, password)

--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,12 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('./authController')
+const {check} = require('express-validator')
 
-router.post('/registration', controller.registration)
+router.post('/registration', [
+    check('username', "Имя пользователя не может быть пустым").notEmpty(),
+    check('password', "Пароль должен быть более 4 и не более 10 символов").isLength({min:4, max: 10})
+], controller.registration)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
 
